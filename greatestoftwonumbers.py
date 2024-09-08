1167329ec0811fef7972516621fcb3cc0d69855d
Varadarajan balaji <87330637+varadarajan2002@users.noreply.github.com>
2024-09-08 20:47:54 +0530

    Add files via upload

---
+++ greatestoftwonumbers.py
@@ -0,0 +1,10 @@
+#To find the greatest of given two numbers
+
+num1=int(input("enter the number:"))
+
+num2=int(input("enter the number:"))
+
+if num1>num2:
+    print(f"The greatest number is{num1}")
+else:
+    print(f"The greates number is {num2}")
